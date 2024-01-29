89fdb982bb94e61a0215a064ae83857009296e8b
Joey <gibbonsjoey95@gmail.com>
2024-01-28 21:09:47 -0600

    set up basic 4 cases

---
+++ script.js
@@ -0,0 +1,20 @@
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+const subtract = (num1, num2) => {
+  return num1 - num2;
+};
+
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+
+const divide = (num1, num2) => {
+  return num1 / num2;
+};
+
+console.log(add(1, 3));
+console.log(subtract(3, 1));
+console.log(multiply(2, 3));
+console.log(divide(9,3));
