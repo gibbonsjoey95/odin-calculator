9f49f6a5e920bb805c637c4513ef74065975366f
Joey <gibbonsjoey95@gmail.com>
2024-01-30 20:58:15 -0600

    got main 3 values to populate

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const btns = document.querySelectorAll('button[id^=btn]')
+const ops = document.querySelectorAll('button[id^=op]')
 const display = document.querySelector('.display')
+
+
 
 let firstNumber = ''
 let secondNumber = ''
@@ -7,21 +10,36 @@ let operator = ''
 
 btns.forEach((btn) => {
   btn.addEventListener('click', () => {
-    firstNumber += btn.value
-    display.textContent = firstNumber
+    if(operator === ''){
+      firstNumber += btn.value
+      display.textContent = firstNumber
+      
+      console.log('1 first', firstNumber)
+      console.log('1 op', operator)
+      console.log('1 second', secondNumber)
+    } else {
+      secondNumber += btn.value
+      display.textContent = secondNumber
+          console.log('first', firstNumber)
+          console.log('operator', operator)
+          console.log('second', secondNumber)
+    }
   })
 })
 
-if(operator === ''){
- btns.forEach((btn) => {
+ops.forEach((btn) => {
   btn.addEventListener('click', () => {
-    firstNumber += btn.value
-    display.textContent = firstNumber
-  })
- })  
-}
+    if(firstNumber !== ''){
+      operator = btn.value
 
-console.log(firstNumber)
+      console.log('3', firstNumber)
+      console.log('3', operator)
+      console.log('3', secondNumber)
+    } else {
+      console.log('error')
+    }
+  })
+})
 
 const add = (num1, num2) => {
   return num1 + num2;
