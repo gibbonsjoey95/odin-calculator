813d9d7eddb71978f6a17c5770d91483b9de2f31
Joey <gibbonsjoey95@gmail.com>
2024-01-30 22:05:18 -0600

    finished most of project

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const btns = document.querySelectorAll('button[id^=btn]')
 const ops = document.querySelectorAll('button[id^=op]')
 const display = document.querySelector('.display')
-
+const equalBtn = document.querySelector('#equalBtn')
+const clearBtn = document.querySelector('#clearBtn')
 
 
 let firstNumber = ''
@@ -13,17 +14,10 @@ btns.forEach((btn) => {
     if(operator === ''){
       firstNumber += btn.value
       display.textContent = firstNumber
-      
-      console.log('1 first', firstNumber)
-      console.log('1 op', operator)
-      console.log('1 second', secondNumber)
     } else {
       secondNumber += btn.value
       display.textContent = secondNumber
-          console.log('first', firstNumber)
-          console.log('operator', operator)
-          console.log('second', secondNumber)
-    }
+  }
   })
 })
 
@@ -31,14 +25,36 @@ ops.forEach((btn) => {
   btn.addEventListener('click', () => {
     if(firstNumber !== ''){
       operator = btn.value
-
-      console.log('3', firstNumber)
-      console.log('3', operator)
-      console.log('3', secondNumber)
     } else {
       console.log('error')
     }
   })
+})
+
+equalBtn.addEventListener('click', () => {
+  if(secondNumber === ''){
+    console.log('help')
+    alert('You didnt put any input for your second value')
+    return firstNumber
+  }
+
+  if(secondNumber === '0'){
+    alert('You can not divide a number by 0.')
+  }
+  
+  let number1 = parseInt(firstNumber)
+  let number2 = parseInt(secondNumber)
+
+  firstNumber = Math.round(operate(operator, number1, number2) * 100)/100
+  secondNumber = ''
+  display.textContent = firstNumber
+})
+
+clearBtn.addEventListener('click', () => {
+  firstNumber = ''
+  secondNumber = ''
+  operator = ''
+  display.textContent = ''
 })
 
 const add = (num1, num2) => {
